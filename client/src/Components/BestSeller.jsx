8f3b0d92a7c1c009270373a9500cf65bf368c599
sadak hassan <sadaqsamor99@gmail.com>
2025-10-03 15:20:19 +0300

    compled bestSeller

--- client/src/Components/BestSeller.jsx
+++ client/src/Components/BestSeller.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ProductCard from './ProductCard'
+import { useAppContext } from '../Context/AppContext'
 
 const BestSeller = () => {
+  const {products}= useAppContext()
   return (
     <div className='mt-16'>
       <h1 className='text-2xl md:text-3xl font-medium'>Best Selles</h1>
-      <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 my-6'>
-        <div>
-          <ProductCard/>
+      <div className='gap-6 p-6 m-3'>
+        <div className='grid grid-cols-1 md:grid-cols-4 gap-6 my-6'>
+          {products.filter(product =>product.price>140).map((product,index)=>(
+            <ProductCard key={index} product={product}/>
+          ))}
+          
         </div>
 
       </div>
