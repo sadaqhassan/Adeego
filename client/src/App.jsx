9c3b8c690b53c84ff5b176791f46758b2f366683
sadak hassan <sadaqsamor99@gmail.com>
2025-10-03 09:33:31 +0300

    show Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Toaster} from 'react-hot-toast'
 import Navbar from './Components/Navbar'
+import { useAppContext } from './Context/AppContext'
+import Login from './Components/Login'
 
 
 const App = () => {
+  const {showUserLogin} = useAppContext()
   return (
     <div>
       <Toaster/>
       <Navbar/>
+    {
+      showUserLogin && <Login/>
+    }
       <div>
-
+      
       </div>
     </div>
   )
