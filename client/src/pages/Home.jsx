52f0375ec95ea5ee50db02444caa09bd1ae1c8e4
sadak hassan <sadaqsamor99@gmail.com>
2025-10-03 13:08:47 +0300

    mo..

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../Components/Banner'
+import Catogeries from '../Components/Catogeries'
 
 const Home = () => {
   return (
     <div className='mt-10'>
       <Banner/>
+      <Catogeries/>
     </div>
   )
 }
